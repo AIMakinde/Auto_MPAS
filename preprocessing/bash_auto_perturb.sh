68344ccc5f3397d9e784ba991ba37ab3e9f9e840
AIMakinde <mckynde@gmail.com>
2025-02-03 15:27:18 +0200

    Added a target_locs.perturb files for configuration an ensemble perturbation.

--- preprocessing/bash_auto_perturb.sh
+++ preprocessing/bash_auto_perturb.sh
@@ -2,13 +2,9 @@
 
 fname="era5_prslv_202204070000.netcdf"
 prfx="ensbl"
-ptb_lev=3
+ptb_lev=15
 ptb_time=0
-ptb_lon=19
-ptb_lat=6
 ptb_var="t"
-ptb_val=0.0001
-ptb_inc=0.0001
 nensemble=10
 
 
@@ -20,12 +16,11 @@ do
 	ofilename="${prfx}_${ens}_${fname}"
 	cp ${fname} ${ofilename}
 
-	# Calculate perturbation value
-	# This calculation involves floating point so the integer arithmetic operator $((....)) can not be used
-	ptb_val=$(echo "ptb_val + (ptb_inc * (ens - 1))" | bc )
-	
+	echo "Updating perturbation target to target_locs.perturb${ens}"
+	cp target_locs.perturb${ens} target_locs.perturb
+
 	# perform actual perturbation
-	python -u perturb_nc.py ${ptb_var} ${ptb_val} ${ptb_lat} ${ptb_lon} --level=${ptb_lev} --time=${ptb_time} --processors=1 ${ofilename}
+	python -u perturb_nc.py ${ptb_var} --level=${ptb_lev} --time=${ptb_time} --processors=1 ${ofilename}
 done
 
 
